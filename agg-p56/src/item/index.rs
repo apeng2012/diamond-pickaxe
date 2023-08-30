fc6bf2562dbd60ea8c396870bdc1b81260f42680
apeng2012 <peng89769975@163.com>
2023-08-30 20:59:13 +0800

    update p59

--- agg-p56/src/item/index.rs
+++ agg-p56/src/item/index.rs
@@ -5,14 +5,24 @@ use crate::Todo;
 
 #[inline_props]
 pub fn Item(cx: Scope, _id: i32, todo: Todo) -> Element {
+    let mouse = use_state(cx, || false);
+
     cx.render(rsx! {
         style { include_str!("./index.css") }
-        li { key: "{_id}",
+        li {
+            key: "{_id}",
+            background_color: if **mouse { "#ddd" } else { "white" },
+            onmouseenter: |_| {
+                mouse.set(true);
+            },
+            onmouseleave: |_| {
+                mouse.set(false);
+            },
             label {
                 input { "type": "checkbox", checked: "{todo.done}" }
                 span { "{todo.name}" }
             }
-            button { class: "btn btn-danger", style: "display: none", "shanchu" }
+            button { class: "btn btn-danger", style: if **mouse { "display: block" } else { "display: none" }, "shanchu" }
         }
     })
 }
