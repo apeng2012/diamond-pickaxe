313efe8d5a9e5854524d466ae57c9a2761d118e5
apeng2012 <peng89769975@163.com>
2023-09-09 02:30:17 +0800

    add agg-p65

---
+++ agg-p65/src/main.rs
@@ -0,0 +1,45 @@
+use axum::{response::IntoResponse, routing::get, Router};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct Student {
+    id: String,
+    name: String,
+    age: u8,
+}
+
+async fn json() -> impl IntoResponse {
+    let students = [
+        Student {
+            id: "001".to_string(),
+            name: "tom".to_string(),
+            age: 18,
+        },
+        Student {
+            id: "002".to_string(),
+            name: "jerry".to_string(),
+            age: 19,
+        },
+        Student {
+            id: "003".to_string(),
+            name: "tony".to_string(),
+            age: 20,
+        },
+    ];
+
+    axum::Json(students)
+}
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route("/students", get(json));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
