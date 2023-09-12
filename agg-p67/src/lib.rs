1f4b1908939f2668e5eac17ca6d0f7dd74a51ea8
apeng2012 <peng89769975@163.com>
2023-09-12 21:18:53 +0800

    update p69

--- agg-p67/src/lib.rs
+++ agg-p67/src/lib.rs
@@ -1,15 +1,47 @@
-#![allow(non_snake_case)]
 use dioxus::prelude::*;
+use serde::Deserialize;
 
 mod list;
 mod search;
 
 pub fn app(cx: Scope) -> Element {
+    let users = use_ref(cx, Vec::<GithubUser>::new);
+
     cx.render(rsx! {
         style { include_str!("./css/bootstrap.css") }
         div { class: "container",
-            search::index::search {}
-            list::index::head_list {}
+            search::index::search { users: users }
+            list::index::head_list { users: users }
         }
     })
+}
+
+#[derive(Clone, PartialEq, Deserialize)]
+pub struct GighubUsers {
+    pub total_count: i64,
+    pub incomplete_results: bool,
+    pub items: Vec<GithubUser>,
+}
+
+#[derive(Clone, PartialEq, Deserialize)]
+pub struct GithubUser {
+    pub login: String,
+    pub id: i64,
+    pub node_id: String,
+    pub avatar_url: String,
+    pub gravatar_id: String,
+    pub url: String,
+    pub html_url: String,
+    pub followers_url: String,
+    pub following_url: String,
+    pub gists_url: String,
+    pub starred_url: String,
+    pub subscriptions_url: String,
+    pub organizations_url: String,
+    pub repos_url: String,
+    pub events_url: String,
+    pub received_events_url: String,
+    pub r#type: String,
+    pub site_admin: bool,
+    pub score: f64,
 }
--- agg-p67/src/search/index.rs
+++ agg-p67/src/search/index.rs
@@ -1,8 +1,8 @@
+use crate::{GighubUsers, GithubUser};
 use dioxus::prelude::*;
-use serde::Deserialize;
 
 #[inline_props]
-pub fn search(cx: Scope) -> Element {
+pub fn search<'a>(cx: Scope, users: &'a UseRef<Vec<GithubUser>>) -> Element {
     let keyword = use_ref(cx, || "".to_string());
     let head = use_future(cx, (keyword,), |(keyword,)| {
         get_head(keyword.read().clone())
@@ -21,7 +21,14 @@ pub fn search(cx: Scope) -> Element {
                         *keyword.write() = e.value.clone();
                     }
                 }
-                button { onclick: move |_| {head.value();}, "Search" }
+                button {
+                    onclick: move |_| {
+                        if let Some(Ok(github_users)) = head.value() {
+                            *users.write() = github_users.items.clone();
+                        }
+                    },
+                    "Search"
+                }
             }
         }
     })
@@ -30,34 +37,4 @@ pub fn search(cx: Scope) -> Element {
 async fn get_head(keyword: String) -> Result<GighubUsers, reqwest::Error> {
     let url = format!("https://api.github.com/search/users?q={}", keyword);
     reqwest::get(&url).await?.json().await
-}
-
-#[derive(Clone, PartialEq, Deserialize)]
-struct GighubUsers {
-    pub total_count: i64,
-    pub incomplete_results: bool,
-    pub items: Vec<GithubUser>,
-}
-
-#[derive(Clone, PartialEq, Deserialize)]
-struct GithubUser {
-    pub login: String,
-    pub id: i64,
-    pub node_id: String,
-    pub avatar_url: String,
-    pub gravatar_id: String,
-    pub url: String,
-    pub html_url: String,
-    pub followers_url: String,
-    pub following_url: String,
-    pub gists_url: String,
-    pub starred_url: String,
-    pub subscriptions_url: String,
-    pub organizations_url: String,
-    pub repos_url: String,
-    pub events_url: String,
-    pub received_events_url: String,
-    pub r#type: String,
-    pub site_admin: bool,
-    pub score: f64,
 }
