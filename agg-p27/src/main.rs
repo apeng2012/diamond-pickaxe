55a3468c5d1c7dff369a27619be67b1c10a4ec02
apeng2012 <peng89769975@163.com>
2024-12-28 18:58:11 +0800

    p27 upgrade to dioxus6

--- agg-p27/src/main.rs
+++ agg-p27/src/main.rs
@@ -1,20 +1,20 @@
-#![allow(non_snake_case)]
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus_web::launch(app);
+    dioxus::launch(App);
 }
 
-fn app(cx: Scope) -> Element {
-    let input1 = use_state(cx, || "".to_string());
-    let input2 = use_state(cx, || "".to_string());
+#[component]
+fn App() -> Element {
+    let mut input1 = use_signal(|| "".to_string());
+    let mut input2 = use_signal(|| "".to_string());
 
-    cx.render(rsx! {
+    rsx! {
         div {
             input {
                 placeholder: "点击按钮提示数据",
                 value: "{input1}",
-                oninput: move |e| input1.set(e.value.clone())
+                oninput: move |e| input1.set(e.value()),
             }
             button { onclick: move |_| gloo_dialogs::alert(&format!("{}", input1)),
                 "点击我提示左侧的数据"
@@ -22,9 +22,9 @@ fn app(cx: Scope) -> Element {
             input {
                 placeholder: "失去焦点提示数据",
                 value: "{input2}",
-                oninput: move |e| input2.set(e.value.clone()),
-                onfocusout: move |_| gloo_dialogs::alert(&format!("{}", input2))
+                oninput: move |e| input2.set(e.value()),
+                onfocusout: move |_| gloo_dialogs::alert(&format!("{}", input2)),
             }
         }
-    })
+    }
 }
