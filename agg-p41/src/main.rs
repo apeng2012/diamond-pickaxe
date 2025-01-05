6d52cf639738c83855ce7619827f81c414de6e38
apeng2012 <peng89769975@163.com>
2025-01-05 19:58:48 +0800

    p41 upgrade to dioxus6

--- agg-p41/src/main.rs
+++ agg-p41/src/main.rs
@@ -1,31 +1,38 @@
-#![allow(non_snake_case)]
 use dioxus::prelude::*;
 
 fn main() {
-    dioxus_web::launch(app);
+    dioxus::launch(App);
 }
 
-fn app(cx: Scope) -> Element {
-    cx.render(rsx! { A {} })
+#[component]
+fn App() -> Element {
+    rsx! {
+        A {}
+    }
 }
 
-fn A(cx: Scope) -> Element {
-    let car_name = use_state(cx, || "奔驰".to_string());
+#[component]
+fn A() -> Element {
+    let mut car_name = use_signal(|| "奔驰".to_string());
 
-    cx.render(rsx! {
+    rsx! {
         div {
             div { "我是A组件" }
             button { onclick: move |_| car_name.set("奥拓".to_string()), "换车" }
-            B { car_name: (**car_name).clone() }
+            B { car_name: car_name() }
         }
-    })
+    }
 }
 
-#[derive(PartialEq, Props)]
+#[derive(PartialEq, Props, Clone)]
 struct BProps {
+    #[props(into)]
     car_name: String,
 }
 
-fn B(cx: Scope<BProps>) -> Element {
-    cx.render(rsx! { div { "我是B组件，接受到的车是{cx.props.car_name}" } })
+#[component]
+fn B(props: BProps) -> Element {
+    rsx! {
+        div { "我是B组件，接受到的车是{props.car_name}" }
+    }
 }
